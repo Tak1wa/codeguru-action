0da92b0812e5bf3a41572e1a6ed15dbb30ed5673
iwasatakahito <iwasa.takahito@classmethod.jp>
2021-09-24 01:11:08 +0900

    push java code.

--- app.py
+++
@@ -1,8 +0,0 @@
-import boto3
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    client = boto3.client('iam', aws_access_key_id='AAAAAAAAAAAAAAAAAAA', aws_secret_access_key='BBBBBBBBBBBBBBBBBBBBBBBBBb')
-    return "Hello, World!!!!!"
