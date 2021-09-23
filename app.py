ccac21fbbddad9458084ff31bc8649df2e290d9c
iwasatakahito <iwasa.takahito@classmethod.jp>
2021-09-24 01:01:43 +0900

    hoge

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+import boto3
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    client = boto3.client('iam', aws_access_key_id='AAAAAAAAAAAAAAAAAAA', aws_secret_access_key='BBBBBBBBBBBBBBBBBBBBBBBBBb')
     return "Hello, World!!!!!"
